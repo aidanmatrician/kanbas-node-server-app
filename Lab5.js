14f01262155f58ced365a063a84f5ac9209287f7
Aidan Matrician <aidanmatrician@gmail.com>
2024-04-02 15:10:40 -0400

    labs completed

--- Lab5.js
+++ Lab5.js
@@ -18,15 +18,24 @@ const todos = [
 
 
 const Lab5 = (app) => {
-  app.get("/a5/todos/create", (req, res) => {
+  app.post("/a5/todos", (req, res) => {
     const newTodo = {
+      ...req.body,
       id: new Date().getTime(),
-      title: "New Task",
-      completed: false,
     };
     todos.push(newTodo);
-    res.json(todos);
+    res.json(newTodo);
   });
+  app.put("/a5/todos/:id", (req, res) => {
+    const { id } = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    todo.title = req.body.title;
+    todo.description = req.body.description;
+    todo.due = req.body.due;
+    todo.completed = req.body.completed;
+    res.sendStatus(200);
+  });
+
   app.get("/a5/todos/:id/completed/:completed", (req, res) => {
     const { id, completed } = req.params;
     const todo = todos.find((t) => t.id === parseInt(id));
@@ -45,15 +54,13 @@ const Lab5 = (app) => {
     todo.title = title;
     res.json(todos);
   });
-  app.get("/a5/todos/:id/delete", (req, res) => {
+  app.delete("/a5/todos/:id", (req, res) => {
     const { id } = req.params;
     const todo = todos.find((t) => t.id === parseInt(id));
-    const todoIndex = todos.indexOf(todo);
-    if (todoIndex !== -1) {
-      todos.splice(todoIndex, 1);
-    }
-    res.json(todos);
+    todos.splice(todos.indexOf(todo), 1);
+    res.sendStatus(200);
   });
+
   app.get("/a5/todos/:id", (req, res) => {
     const { id } = req.params;
     const todo = todos.find((t) => t.id === parseInt(id));
