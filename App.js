287ff1eb7924e51a52a2fce68d26b04d8d14fe2d
Aidan Matrician <aidanmatrician@gmail.com>
2024-04-01 23:53:32 -0400

    init

---
+++ App.js
@@ -0,0 +1,8 @@
+import Hello from "./Hello.js";
+import Lab5 from "./Lab5.js";
+import express from 'express';
+const app = express();
+app.use(express.json());
+Lab5(app);
+Hello(app);
+app.listen(4000);
