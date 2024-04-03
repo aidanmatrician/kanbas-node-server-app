bb56e43e209e630797780deb4cd03705f7de6a85
Aidan Matrician <aidanmatrician@gmail.com>
2024-04-02 23:00:14 -0400

    complete

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
+import CourseRoutes from "./Kanbas/courses/routes.js";
 import express from 'express';
 import cors from "cors";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+ModuleRoutes(app);
+CourseRoutes(app);
 Lab5(app);
 Hello(app);
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
---
+++ Kanbas/modules/routes.js
@@ -0,0 +1,21 @@
+import db from "../Database/index.js";
+function ModuleRoutes(app) {
+  app.get("/api/courses/:cid/modules", (req, res) => {
+    const { cid } = req.params;
+    const modules = db.modules
+      .filter((m) => m.course === cid);
+    res.send(modules);
+  });
+
+  app.put("/api/modules/:mid", (req, res) => {
+    const { mid } = req.params;
+    const moduleIndex = db.modules.findIndex(
+      (m) => m._id === mid);
+    db.modules[moduleIndex] = {
+      ...db.modules[moduleIndex],
+      ...req.body
+    };
+    res.sendStatus(204);
+  });
+}
+export default ModuleRoutes;
